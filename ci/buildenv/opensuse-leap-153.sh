afc3ef0d95cb33c7d4898364c3c2802779df253c
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2023-01-22 17:08:45 -0500

    https://gitlab.com/virt-viewer/virt-viewer + patch

---
+++ ci/buildenv/opensuse-leap-153.sh
@@ -0,0 +1,48 @@
+# THIS FILE WAS AUTO-GENERATED
+#
+#  $ lcitool manifest ci/manifest.yml
+#
+# https://gitlab.com/libvirt/libvirt-ci
+
+function install_buildenv() {
+    zypper update -y
+    zypper install -y \
+           bash-completion \
+           ca-certificates \
+           ccache \
+           cppi \
+           gcc \
+           gettext-runtime \
+           git \
+           glib2-devel \
+           glibc-locale \
+           gtk-vnc-devel \
+           gtk3-devel \
+           icoutils \
+           libgovirt-devel \
+           librest-devel \
+           libtool \
+           libvirt-devel \
+           libvirt-glib-devel \
+           libxml2 \
+           libxml2-devel \
+           make \
+           ninja \
+           pkgconfig \
+           python3-pip \
+           python3-setuptools \
+           python3-wheel \
+           rpm-build \
+           spice-gtk-devel \
+           vte-devel
+    rpm -qa | sort > /packages.txt
+    mkdir -p /usr/libexec/ccache-wrappers
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
+    /usr/bin/pip3 install meson==0.56.0
+}
+
+export CCACHE_WRAPPERSDIR="/usr/libexec/ccache-wrappers"
+export LANG="en_US.UTF-8"
+export MAKE="/usr/bin/make"
+export NINJA="/usr/bin/ninja"
---
+++ ci/buildenv/opensuse-tumbleweed.sh
@@ -0,0 +1,45 @@
+# THIS FILE WAS AUTO-GENERATED
+#
+#  $ lcitool manifest ci/manifest.yml
+#
+# https://gitlab.com/libvirt/libvirt-ci
+
+function install_buildenv() {
+    zypper dist-upgrade -y
+    zypper install -y \
+           bash-completion \
+           ca-certificates \
+           ccache \
+           cppi \
+           gcc \
+           gettext-runtime \
+           git \
+           glib2-devel \
+           glibc-locale \
+           gtk-vnc-devel \
+           gtk3-devel \
+           icoutils \
+           libgovirt-devel \
+           librest-devel \
+           libtool \
+           libvirt-devel \
+           libvirt-glib-devel \
+           libxml2 \
+           libxml2-devel \
+           make \
+           meson \
+           ninja \
+           pkgconfig \
+           rpm-build \
+           spice-gtk-devel \
+           vte-devel
+    rpm -qa | sort > /packages.txt
+    mkdir -p /usr/libexec/ccache-wrappers
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
+}
+
+export CCACHE_WRAPPERSDIR="/usr/libexec/ccache-wrappers"
+export LANG="en_US.UTF-8"
+export MAKE="/usr/bin/make"
+export NINJA="/usr/bin/ninja"
